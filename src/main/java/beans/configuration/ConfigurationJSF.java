8e66d08f83810fc1689d684c7fccdeda3f6fc6f4
mleandrocardenas <michaelcardenas199@gmail.com>
2023-04-06 23:10:28 -0500

    plantilla para proyecto en jsf

---
+++ src/main/java/beans/configuration/ConfigurationJSF.java
@@ -0,0 +1,17 @@
+package beans.configuration;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author Michael Cardenas
+ */
+// Activa el CDI para los beans a agregar V JSF_2_3
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+@ApplicationScoped
+public class ConfigurationJSF {
+    
+    
+    
+}
